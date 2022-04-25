7aa54f63aedac99131e67fdfea3d9f860b54befe
RabiLarkLouie <louierabi@gmail.com>
2022-04-25 15:38:20 +0800

    song4 has been updated

---
+++ song4.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> FavSong List </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to FavSong List</h1>
+                <p class="lead">Songs that reminisce your life.  </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Closer You And I</h2>";
+                        echo "<p class='lead '>Gino Padilla</p>";
+                
+                        echo "<p>.....C..........Em7.............Am...</p>";
+                        echo "<p> Hey, there's a look in your eyes </p>";
+                        echo "<p> .........F.........................G.....</p>";
+                        echo "<p> Must be love at first sight </p>";
+                        echo "<p> ....C...........Em7............Am....</p>";
+                        echo "<p> You were just part of a dream</p>";
+                        echo "<p> ....F............................G.........</p>";
+                        echo "<p> Nothing more so it seemed </p>";
+                        echo "<p> ......Dm.....................Em........</p>";
+                        echo "<p> But my love couldn't wait much longer </p>";
+                        echo "<p> .....Am...........................D7..........</p>";
+                        echo "<p> Just can't forget the picture of your smile</p>";
+                        echo "<p> ....F...........Dm..........</p>";
+                        echo "<p> 'Coz every time I close my eyes</p>";
+                        echo "<p> ................G7.....</p>";
+                        echo "<p> You come alive </p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p> S......E.....................Am........</p>";
+                        echo "<p> The closer I get to touching you</p>";
+                        echo "<p> .......D7....................G7........</p>";
+                        echo "<p> The closer I get to loving you </p>";
+                        echo "<p> .....................C......</p>";
+                        echo "<p> Give it a time</p>";
+                        echo "<p> ......................F.........</p>";
+                        echo "<p> Just a little more time</p>";
+                        echo "<p> ...........Dm...............G7......</p>";
+                        echo "<p> We'll be together </p>";
+                        echo "<p> .....................E.......</p>";
+                        echo "<p> Every little smile</p>";
+                        echo "<p> .....................Am.......</p>";
+                        echo "<p> That special smile</p>";
+                        echo "<p> ............................D7......</p>";
+                        echo "<p> The twinkle in your eye </p>";
+                        echo "<p> ............................G7......</p>";
+                        echo "<p> In a little while</p>";
+                        echo "<p> .....................C...........</p>";
+                        echo "<p> Give it a time </p>";
+                        echo "<p> ............................F....... </p>";
+                        echo "<p> Just a little more time </p>";
+                        echo "<p> ..................Dm.......G7.......</p>";
+                        echo "<p> So we can get closer</p>";
+                        echo "<p> You and I</p>";
+                        echo "<br/>";
+
+                        
+                        echo "<p>.....C..........Em7.............Am...</p>";
+                        echo "<p> Then could I love you more  </p>";
+                        echo "<p> .........F.........................G.....</p>";
+                        echo "<p> So much stronger than before  </p>";
+                        echo "<p> ....C...........Em7............Am....</p>";
+                        echo "<p> Why does it seem like a dream </p>";
+                        echo "<p> ....F........................G.........</p>";
+                        echo "<p> So much more so it seems  </p>";
+                        echo "<p> ............Dm...................Em........</p>";
+                        echo "<p> I guess I found my inspiration </p>";
+                        echo "<p> .................Am.............D7..........</p>";
+                        echo "<p> With just one smile, you take my breath away </p>";
+                        echo "<p> ....................F.......</p>";
+                        echo "<p> So hold me close </p>";
+                        echo "<p> ............Dm.......G7.....</p>";
+                        echo "<p> And say you'll stay with me now </p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p> S......E.....................Am........</p>";
+                        echo "<p> The closer I get to touching you</p>";
+                        echo "<p> .......D7....................G7........</p>";
+                        echo "<p> The closer I get to loving you </p>";
+                        echo "<p> .....................C......</p>";
+                        echo "<p> Give it a time</p>";
+                        echo "<p> ......................F.........</p>";
+                        echo "<p> Just a little more time</p>";
+                        echo "<p> ...........Dm...............G7......</p>";
+                        echo "<p> We'll be together </p>";
+                        echo "<p> .....................E.......</p>";
+                        echo "<p> Every little smile</p>";
+                        echo "<p> .....................Am.......</p>";
+                        echo "<p> That special smile</p>";
+                        echo "<p> ............................D7......</p>";
+                        echo "<p> The twinkle in your eye </p>";
+                        echo "<p> ............................G7......</p>";
+                        echo "<p> In a little while</p>";
+                        echo "<p> .....................C...........</p>";
+                        echo "<p> Give it a time </p>";
+                        echo "<p> ............................F....... </p>";
+                        echo "<p> Just a little more time </p>";
+                        echo "<p> ..................Dm.......G7.......</p>";
+                        echo "<p> So we can get closer</p>";
+                        echo "<p> You and I</p>";
+                        echo "<br/>";
+
+                        ?>
+                        Back to Homepage?<a href="index.php">Click here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lark Louie Rabi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
