61c9fffe07b29f0e6ece28ea4da525c4a08e2633
RabiLarkLouie <louierabi@gmail.com>
2022-04-25 15:39:19 +0800

    song5 has been updated

---
+++ song5.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> FavSong List </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to FavSong List</h1>
+                <p class="lead">Songs that reminisce your life.  </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>I Can't Let Go</h2>";
+                        echo "<p class='lead '>Air Supply</p>";
+                
+                        echo "<p> ...F.......................Bb....</p>";
+                        echo "<p> I have the ring that needs your finger</p>";
+                        echo "<p> ...Am................Dm..........Bb...</p>";
+                        echo "<p> I want you always to remember Oh-oh</p>";
+                        echo "<p> ..............C.............F.........</p>";
+                        echo "<p> When she looks at me</p>";
+                        echo "<p> ....D..................E.......</p>";
+                        echo "<p> I want you to be a part of my dreams</p>";
+                        echo "<p> ...F.......................Bb....</p>";
+                        echo "<p> I was so lonely here without you</p>";
+                        echo "<p> ....Am.....................Dm...</p>";
+                        echo "<p> I was so cold just like a statue,</p>";
+                        echo "<p> ..Bb....</p>";
+                        echo "<p> Oh-oh</p>";
+                        echo "<p> .........C.............F......</p>";
+                        echo "<p> Being with you has made me complete</p>";
+                        echo "<p> ..Bb.........................</p>";
+                        echo "<p> I can feel your loving touch inside me </p>";
+                        echo "<p> .........Em.............C#m.........D..</p>";
+                        echo "<p> And she doesn't judge the man</p>";
+                        echo "<p> ..Bb.........................</p>";
+                        echo "<p> stay close to me</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p>.....F................Dm........</p>";
+                        echo "<p>Every day and every night</p>";
+                        echo "<p> ....Bb....................C.....</p>";
+                        echo "<p> I never thought your love could feel so right</p>";
+                        echo "<p> .....F...............Dm......</p>";
+                        echo "<p> Every day and through the night</p>";
+                        echo "<p> ....Bb....................C.....</p>";
+                        echo "<p> Let me be the one to hold you tight</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ..........D....................Gm....</p>";
+                        echo "<p> You're a part of me, don't you know </p>";
+                        echo "<br/>";
+                        
+                        echo "<p> ...F.......................Bb....</p>";
+                        echo "<p> Would you give me just a lifetime</p>";
+                        echo "<p> ...Am................Dm..........Bb...</p>";
+                        echo "<p> I'II stand beside you through the years</p>";
+                        echo "<p> ..................Bb......</p>";
+                        echo "<p> that come and go</p>";
+                        echo "<p> ..............C.............F.........</p>";
+                        echo "<p> Nothing at all could stand in our way</p>";
+                        echo "<p> ....Bb..................C.......</p>";
+                        echo "<p> I can feel your answer deep inside me</p>";
+                        echo "<p> .........Bb.......</p>";
+                        echo "<p> stay close to me</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p>.....F................Dm........</p>";
+                        echo "<p>Every day and every night</p>";
+                        echo "<p> ....Bb....................C.....</p>";
+                        echo "<p> I never thought your love could feel so right</p>";
+                        echo "<p> .....F...............Dm......</p>";
+                        echo "<p> Every day and through the night</p>";
+                        echo "<p> ....Bb....................C.....</p>";
+                        echo "<p> Let me be the one to hold you tight</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ..........D....................Gm....</p>";
+                        echo "<p> You're a part of me, don't you know </p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ...F......Dm......Bb....C......</p>";
+                        echo "<p>I can't let go</p>";
+                        echo "<p> ..........D....................Gm....</p>";
+                        echo "<p> You're a part of me, don't you know </p>";
+                        echo "<br/>";
+                        
+                        ?>
+
+                        Back to Homepage?<a href="index.php">Click here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lark Louie Rabi</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
