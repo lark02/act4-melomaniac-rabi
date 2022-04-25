fe5fa7cebe0361961a52e94b4d9c6b1679e9a42a
RabiLarkLouie <louierabi@gmail.com>
2022-04-25 14:26:45 +0800

    songs has been uploaded

---
+++ song1.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> FavSong List </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to FavSong List</h1>
+                <p class="lead">Songs that reminisce your life. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Having You Near Me</h2>";
+                        echo "<p class='lead '>Air Supply</p>";
+                
+                        echo "<p>F.......................G.............Em.............Am </p>";
+                        echo "<p>I came to you and never asked too much</p>";
+                        echo "<p>F...........................G.......G...G/F</p>";
+                        echo "<p> Wondering what you would say</p>";
+                        echo "<p> Em.......................Am</p>";
+                        echo "<p>Hoping you'd understand</p>";
+                        echo "<p>F..........................G</p>";
+                        echo "<p>It's not a role I usually play</p>";
+                        echo "<p>F..........................G.........G...G/F.....Em............Am</p>";
+                        echo "<p>Don't speak too much of what's been going on</p>";
+                        echo "<p>F................................G...............G/F</p>";
+                        echo "<p>The past is over and gone</p>";
+                        echo "<p>Em.................</p>";
+                        echo "<p>Give me a troubled mind</p>";
+                        echo "<p>.....................Dm</p>";
+                        echo "<p>You know it's due</p>";
+                        echo "<p>.......F...........G.........</p>";
+                        echo "<p>I can do so mucH for you</p>";
+                        echo "<br/>";
+
+                        echo "<p>........C.................Em.......</p>";
+                        echo "<p> Having you near me, holding you near me</p>";
+                        echo "<p> Dm..............Am...........F.....</p>";
+                        echo "<p> I want you stay and never go away</p>";
+                        echo "<p> .................G..............</p>";
+                        echo "<p> It's so right</p>";
+                        echo "<p> C..............................Em............</p>";
+                        echo "<p> Having you near me, holding you near me</p>";
+                        echo "<p> Dm7................Am..........F.................G</p>";
+                        echo "<p> I'll love you tonight, it feels so right</p>";
+                        echo "<p>........C.......</p>";
+                        echo "<p>Feels so right</p>";
+                        echo "<br/>";
+
+                        echo "<p> F................G...........G/F.......Em........Am..</p>";
+                        echo "<p> You're brave to say that you get lost in love</p>";
+                        echo "<p>............ F...........G............G/F........</p>";
+                        echo "<p> But you opened your heart to me</p>";
+                        echo "<p> ..Em...............................Am...</p>";
+                        echo "<p> Underneath all you feel you know</p>";
+                        echo "<p> .....F.....................G.............</p>";
+                        echo "<p>How deep our love could be</p>";
+                        echo "<p> ...F.......G.........G/F............Em......Am...</p>";
+                        echo "<p> Tonight we'll touch until it's time to go</p>";
+                        echo "<p> Underneath all you feel you know</p>";
+                        echo "<p>............ F...........G............G/F........</p>";
+                        echo "<p>Then I'm leaving it up to you</p>";
+                        echo "<p> ..Em............................Am..............Dm..</p>";
+                        echo "<p> Even a fool would know that I'm not through</p>";
+                        echo "<p>................. F...................G.......</p>";
+                         echo "<p>I can do so mucH for you I want you</p>";
+                        echo "<br/>";
+
+                        echo "<p>........C.................Em.......</p>";
+                        echo "<p> Having you near me, holding you near me</p>";
+                        echo "<p> Dm..............Am...........F.....</p>";
+                        echo "<p> I want you stay and never go away</p>";
+                        echo "<p> .................G..............</p>";
+                        echo "<p> It's so right</p>";
+                        echo "<p> C..............................Em............</p>";
+                        echo "<p> Having you near me, holding you near me</p>";
+                        echo "<p> Dm7................Am..........F.................G</p>";
+                        echo "<p> I'll love you tonight, it feels so right</p>";
+                        echo "<p>........C.......</p>";
+                        echo "<p>Feels so right</p>";
+                        echo "<br/>";
+
+
+
+                        echo "<p> [Adlib]</p>";
+                        echo "<p> F....G......G......G/F......Em....AM..</p>";
+                        echo "<p> DM...F......G</p>";
+                        echo "<br/>";
+
+
+
+                        echo "<p>........C.................Em.......</p>";
+                        echo "<p> Having you near me, holding you near me</p>";
+                        echo "<p> Dm..............Am...........F.....</p>";
+                        echo "<p> I want you stay and never go away</p>";
+                        echo "<p> .................G..............</p>";
+                        echo "<p> It's so right</p>";
+                        echo "<p> C..............................Em............</p>";
+                        echo "<p> Having you near me, holding you near me</p>";
+                        echo "<p> Dm7................Am..........F.................G</p>";
+                        echo "<p> I'll love you tonight, it feels so right</p>";
+                        echo "<p>........C.......</p>";
+                        echo "<p>Feels so right</p>";
+                        echo "<br/>";
+                        
+
+
+                        ?>
+                        
+                        Back to Homepage?<a href="index.php">Click here!</a>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ryan Clifford Perez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ song3.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> FavSong List </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to FavSong List</h1>
+                <p class="lead">Songs that reminisce your life. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Let Me Be The One</h2>";
+                        echo "<p class='lead '>Jimmy Bondoc</p>";
+                
+                        echo "<p> ...........A............D...... </p>";
+                        echo "<p> Somebody told me you were leavin’</p>";
+                        echo "<p> ..........A.....D.........</p>";
+                        echo "<p> I didn't know</p>";
+                        echo "<p> ...A...........................</p>";
+                        echo "<p> Somebody told me you’re unhappy</p>";
+                        echo "<p> .............C#m............F#m.........</p>";
+                        echo "<p> But it doesn’t show</p>";
+                        echo "<p> ...D..............C#m..........Em.......</p>";
+                        echo "<p> Somebody told me that you don’t want me no more</p>";
+                        echo "<p> ...E............................</p>";
+                        echo "<p> No, So you’re walkin’ out the door</p>";
+                        echo "<p> ...A..............................D...</p>";
+                        echo "<p> Nobody told me you’ve been cryin’</p>";
+                        echo "<p> ...A..................</p>";
+                        echo "<p> Every night</p>";
+                        echo "<p> ..A...........................D....</p>";
+                        echo "<p> Nobody told me you’d been dyin’</p>";
+                        echo "<p> .............C#m............F#m.........</p>";
+                        echo "<p> But didn’t want to fight</p>";
+                        echo "<p> ...D..............C#m..........Em.......</p>";
+                        echo "<p> Nobody told me that you fell out of love from me</p>";
+                        echo "<p> ...E..................A..........</p>";
+                        echo "<p> So I’m settin’ you free</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p> ...D........................C#m...</p>";
+                        echo "<p>Let me be the one to break it up</p>";
+                        echo "<p> ....Em......................A.....</p>";
+                        echo "<p> So you won’t have to make excuses</p>";
+                        echo "<p> ....D.......................C#m......</p>";
+                        echo "<p>We don’t need to find a set up where</p>";
+                        echo "<p> ...Em.........................A......</p>";
+                        echo "<p> Someone wins and someone loses</p>";
+                        echo "<p> ...D..................C#............</p>";
+                        echo "<p> We just have to say our love was true</p>";
+                        echo "<p> .........F#m.....................B...</p>";
+                        echo "<p> But has now become a lie</p>";
+                        echo "<p> ...D...........C#m................B......</p>";
+                        echo "<p> So I’m tellin’ you I love you one last time</p>";
+                        echo "<p> ....D-A/C#-B....</p>";
+                        echo "<p> And goodbye</p>";
+                        echo "<br/>";
+
+                        echo "<p> ...A............................</p>";
+                        echo "<p> Somebody told me you still loved me</p>";
+                        echo "<p> ...........A............D...... </p>";
+                        echo "<p> Don’t know why’</p>";
+                        echo "<p> ..........A.....D.........</p>";
+                        echo "<p> I didn't know</p>";
+                        echo "<p> ...A...........................</p>";
+                        echo "<p> Nobody told me that you only</p>";
+                        echo "<p> .............C#m............F#m.........</p>";
+                        echo "<p> Needed time to fly</p>";
+                        echo "<p> ...D..............C#m..........Em.......</p>";
+                        echo "<p> Somebody told me that you want to come back when</p>";
+                        echo "<p> ...E............................</p>";
+                        echo "<p> Our love is real again</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p> ...D........................C#m...</p>";
+                        echo "<p>Let me be the one to break it up</p>";
+                        echo "<p> ....Em......................A.....</p>";
+                        echo "<p> So you won’t have to make excuses</p>";
+                        echo "<p> ....D.......................C#m......</p>";
+                        echo "<p>We don’t need to find a set up where</p>";
+                        echo "<p> ...Em.........................A......</p>";
+                        echo "<p> Someone wins and someone loses</p>";
+                        echo "<p> ...D..................C#............</p>";
+                        echo "<p> We just have to say our love was true</p>";
+                        echo "<p> .........F#m.....................B...</p>";
+                        echo "<p> But has now become a lie</p>";
+                        echo "<p> ...D...........C#m................B......</p>";
+                        echo "<p> So I’m tellin’ you I love you one last time</p>";
+                        echo "<p> ....D-A/C#-B....</p>";
+                        echo "<p> And goodbye</p>";
+                        echo "<br/>";
+
+                        echo "<p> ...........E............D...... </p>";
+                        echo "<p> Just turn around and walk away</p>";
+                        echo "<p> ..........C#m............Em........D........E....</p>";
+                        echo "<p> You don’t have to live like this... Ohhhh</p>";
+                        echo "<p> ............C#m..........F#m.......</p>";
+                        echo "<p> If you love me still then stay</p>";
+                        echo "<p> ...........D......................C#....</p>";
+                        echo "<p> Don’t keep me waiting for that final kiss</p>";
+                        echo "<p> ........F#m...................C#m.......</p>";
+                        echo "<p> We can work together through this test</p>";
+                        echo "<p> .....D..................C#m.......</p>";
+                        echo "<p> Or we can work through it apart</p>";
+                        echo "<p> .......C......................F....</p>";
+                        echo "<p> I just need to get this off my chest</p>";
+                        echo "<p> .............Bm................E.......</p>";
+                        echo "<p> That you will always have my heart</p>";
+                        echo "<br/>";
+
+                        echo "<p> CHORUS</p>";
+                        echo "<p> ...D........................C#m...</p>";
+                        echo "<p>Let me be the one to break it up</p>";
+                        echo "<p> ....Em......................A.....</p>";
+                        echo "<p> So you won’t have to make excuses</p>";
+                        echo "<p> ....D.......................C#m......</p>";
+                        echo "<p>We don’t need to find a set up where</p>";
+                        echo "<p> ...Em.........................A......</p>";
+                        echo "<p> Someone wins and someone loses</p>";
+                        echo "<p> ...D..................C#............</p>";
+                        echo "<p> We just have to say our love was true</p>";
+                        echo "<p> .........F#m.....................B...</p>";
+                        echo "<p> But has now become a lie</p>";
+                        echo "<p> ...D...........C#m................B......</p>";
+                        echo "<p> So I’m tellin’ you I love you one last time</p>";
+                        echo "<p> ....D-A/C#-B....</p>";
+                        echo "<p> And goodbye...</p>";
+                        echo "<br/>";
+
+                        ?>
+                        Back to Homepage?<a href="index.php">Click here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Ryan Clifford Perez</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
